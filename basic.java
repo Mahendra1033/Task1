ac21b5b8c6ffcc24b34901ca4c0b0cb6a4896340
Mahendra K <86547737+Mahendra1033@users.noreply.github.com>
2022-03-28 18:43:25 +0530

    Added substract method

--- basic.java
+++ basic.java
@@ -1,5 +1,9 @@
 public class Basic{
+  public static void substract(){
+     System.out.println("SubstractMethod");
+  }
   public static void main(String[] args){
+    Basic.substract();
     int i = 10;
     System.out.println("Hello World "+i);
   }
