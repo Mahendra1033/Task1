9c7800c36d80bc0929f065bedad04588b1e76889
Mahendra K <86547737+Mahendra1033@users.noreply.github.com>
2022-03-28 18:41:08 +0530

    Added add() method

--- basic.java
+++ basic.java
@@ -1,5 +1,9 @@
 public class Basic{
+  public static void add(){
+     System.out.println("Add Method");
+  }
   public static void main(String[] args){
+    Basic.add();
     int i = 10;
     System.out.println("Hello World "+i);
   }
